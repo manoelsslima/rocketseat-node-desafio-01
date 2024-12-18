64c9e1d7812445f4fa7affcdbe662721af77dd68
Manoel Lima <manoelsslima@yahoo.com.br>
2024-12-18 01:13:23 -0400

    feat: create task

--- src/server.js
+++ src/server.js
@@ -1,16 +1,57 @@
 import http from 'node:http';
+import { randomUUID } from 'node:crypto';
 
 const tasks = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
     const { method, url } = req;
+
+    const buffers = [];
+
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch {
+        req.body = null
+    }
 
     if (method === 'GET' && url === '/tasks') {
         return res
             .setHeader('Content-Type', 'application/json')
             .writeHead(200)
             .end(JSON.stringify(tasks));
+    }
+
+    if (method === 'POST' && url === '/tasks') {
+
+        const { title, description } = req.body;
+
+        tasks.push({
+            id: randomUUID(),
+            title,
+            description,
+            created_at: new Date()
+        });
+
+        return res
+            .writeHead(201)
+            .end('Criação de tarefa');
+    }
+
+    if (method === 'PUT' && url === '/tasks/:id') {
+
+    }
+
+    if (method === 'DELETE' && url === '/tasks/:id') {
+
+    }
+
+    if (method === 'PATCH' && url === '/tasks/:id/complete') {
+        
     }
 
     return res
